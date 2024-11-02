661c06f8b257cf297faa1083e4b88aafab4da2bd
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-11-02 01:38:27 +0000

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,15 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { create } from 'zustand'
 import React from 'react';
+import { useConversationStore } from './store/conversation';
+import type { Message } from './store/conversation';
 
 const recentSearches = [
   'Test 1',
   'Test 2',
   'Test 3',
 ];
-
-export type Message = {
-  role: "user" | "assistant";
-  content: string;
-};
-
-interface ConversationStore {
-  conversation: Message[];
-  setConversation: (newMessage: Message) => void;
-  updateLastMessage: (content: string) => void;
-  question: string;
-  setQuestion: (newQuestion: string) => void;
-}
-
-export const useConversationStore = create<ConversationStore>((set) => ({
-  conversation: [],
-  setConversation: (newMessage: Message) => set((state) => ({
-    conversation: [...state.conversation, newMessage]
-  })),
-  updateLastMessage: (content: string) => set((state) => ({
-    conversation: state.conversation.map((msg, index) => 
-      index === state.conversation.length - 1 ? { ...msg, content } : msg
-    )
-  })),
-  question: "",
-  setQuestion: (newQuestion: string) => set((state) => ({
-    question: newQuestion,
-  })),
-}));
 
 export default function Home() {
   const router = useRouter();
